f867ee73498974d2619c31a65b0605722236cd8d
magrim <marczuk@live.it>
2018-12-13 12:46:01 +0100

    Adjust position estimation node to new RoadMarkings specs

--- src/selfie_path_planner/include/path_planner/path_planner.h
+++ src/selfie_path_planner/include/path_planner/path_planner.h
@@ -6,14 +6,6 @@
 #include "nav_msgs/Path.h"
 #include <path_planner/polyfit.hpp>
 
-#define KUROKESU 1
-
-#if KUROKESU
-    #define MAT_HEIGHT 480
-    #define MAT_WIDTH 640
-#endif
-
-void RoadMarkings_to_cloud(const selfie_msgs::RoadMarkings::ConstPtr& msg, sensor_msgs::PointCloud& points_preview);
-void poly_to_path(poly polyline,nav_msgs::Path& path);
+void poly_to_path(poly polyline,nav_msgs::Path& path,bool reversed);
 
 #endif // PATH_PLANNER_H
