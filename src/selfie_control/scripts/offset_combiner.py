e3a6f2e26b6baca802e41cfcafabaaab4e0ab7eb
Adam Gotlib <gotlib.adam+dev@gmail.com>
2019-01-26 14:39:02 +0100

    Rename `angular_offset` to `heading_offset`

--- src/selfie_control/scripts/offset_combiner.py
+++ src/selfie_control/scripts/offset_combiner.py
@@ -4,12 +4,12 @@ import rospy
 
 from std_msgs.msg import Float64
 from dynamic_reconfigure.server import Server
-from selfie_control.cfg import AngularCoeffConfig
+from selfie_control.cfg import HeadingCoeffConfig
 
 UPDATE_RATE = 50
 
 position_offset = 0.0
-angular_offset = 0.0
+heading_offset = 0.0
 
 def config_callback(config, level):
     global L
@@ -23,29 +23,29 @@ def position_offset_callback(msg):
     global position_offset
     position_offset = msg.data
 
-def angular_offset_callback(msg):
-    global angular_offset
-    angular_offset = msg.data
+def heading_offset_callback(msg):
+    global heading_offset
+    heading_offset = msg.data
 
 if __name__ == '__main__':
     rospy.init_node('selfie_offset_combiner')
 
-    srv = Server(AngularCoeffConfig, config_callback)
+    srv = Server(HeadingCoeffConfig, config_callback)
 
     position_offset_sub = rospy.Subscriber('position_offset',
                                             Float64,
                                             position_offset_callback,
                                             queue_size=1)
 
-    angular_offset_sub = rospy.Subscriber('angular_offset',
+    heading_offset_sub = rospy.Subscriber('heading_offset',
                                            Float64,
-                                           angular_offset_callback,
+                                           heading_offset_callback,
                                            queue_size=1)
 
     combined_offset_pub = rospy.Publisher('combined_offset', Float64, queue_size=1)
 
     rate = rospy.Rate(UPDATE_RATE)
     while not rospy.is_shutdown():
-        combined_offset = position_offset + L*angular_offset
+        combined_offset = position_offset + L*heading_offset
         combined_offset_pub.publish(combined_offset)
         rate.sleep()
