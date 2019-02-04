f1ae05e1c8bf17fc9c9bde99153a3abd37b7dca2
AvaRat <marcelos.kalinsi@gmail.com>
2019-02-04 10:52:41 +0100

    odom and drive tpics chacnge

--- src/selfie_park/src/park_server.cpp
+++ src/selfie_park/src/park_server.cpp
@@ -7,8 +7,8 @@ pnh_(pnh),
 as_(nh_, "park",  false),
 visualize(true)
 {
-  pnh_.param<std::string>("odom_topic", odom_topic,"/vesc/odom");
-  pnh_.param<std::string>("ackermann_topic", ackermann_topic,"/sim_drive");
+  pnh_.param<std::string>("odom_topic", odom_topic,"/odom");
+  pnh_.param<std::string>("ackermann_topic", ackermann_topic,"/drive");
   pnh_.param<float>("minimal_start_parking_x", minimal_start_parking_x, -0.13);
   pnh_.param<float>("maximal_start_parking_x", maximal_start_parking_x, 0.0);
   pnh_.param<float>("traffic_lane_marigin",traffic_lane_marigin, 0.05);
